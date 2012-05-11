60b64d6d6099fc8ca23d6f3a8d42e9fe28feded7
Nick <nick@kousu.ca>
2012-05-11 16:34:53 -0400

    dickcloud

--- audio.js
+++ audio.js
@@ -1,9 +1,14 @@
 
 
+/* notes: mediaGroup is for linking playback together, e.g. a video with its attendant audio stream. basically, it's not at all what we need, so leave it null */
+
 game.soundtrack = document.createElement("audio")
 game.soundtrack.setAttribute("id", "soundtrack")
+game.soundtrack.autoplay = true;
+game.soundtrack.loop = true;
+
 game.soundeffects = []
 for(i=0; i<32; i++)
 {
-  game.soundeffects.push(document.createElement("audio"))
+  game.soundeffects.push(document.createElement("audio")) //assumption: loop is false by default
 }
