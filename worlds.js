2d5d18c7eab574fc8ce80c105ea00855b260bab5
Nick <nick@kousu.ca>
2012-05-12 01:19:27 -0400

    worlds

---
+++ worlds.js
@@ -0,0 +1,15 @@
+
+/* worlds is the model for what is happening on a planets
+ * hence covers:
+ *  -resources / terrain
+ *  -
+ *  -
+ *  -
+ */
+
+Worlds = (function() {
+  
+return {
+   
+}
+})();
