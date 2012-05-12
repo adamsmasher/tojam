6cb01148022819dc52614350539464bf360bec28
Nick <nick@kousu.ca>
2012-05-11 22:15:29 -0400

    shit, fucked up

--- util.js
+++ util.js
@@ -1,21 +1,23 @@
-<<<<<<< HEAD
 
+Util = {
 /* a hurr durr javascript you suck */
-function randint(l, u) {
+randint: function(l, u) {
  if(!u) {
    u = l 
    l = 0
  }
  return Math.floor(Math.random()*(u-l) + l)
-}
+},
 
 
 /* for debugging, mostly */
-function sleep(milliseconds) {
+sleep: function(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
     if ((new Date().getTime() - start) > milliseconds){
       break;
     }
   }
+}
+
 }
